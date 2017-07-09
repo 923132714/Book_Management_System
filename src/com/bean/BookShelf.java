eb7e059f7a689b84bccd65a05a5f6e09c760ddd5
logic <923132714@qq.com>
2017-07-09 10:44:37 +0800

    Increase code reuse
    增强代码复用
    Add getBook method, find book object from shlef by id.
    添加getBook方法，通过图书编号获取图书对象。

--- src/com/bean/BookShelf.java
+++ src/com/bean/BookShelf.java
@@ -3,7 +3,6 @@ package com.bean;
 import java.util.Iterator;
 import java.util.LinkedList;
 
-
 /**
  * @author logic
  *
@@ -12,40 +11,55 @@ public class BookShelf {
 	private LinkedList<Book> shelf = new LinkedList<Book>();
 	private int differentBookNumber = 0;
 
+	/*
+	 * 通过调用getBook()
+	 */
 	public boolean addBook(Book book) {
 		boolean flag = false;
-		if (shelf.add(book)) {
-			differentBookNumber++;
+		if (getBook(book.getBookId()) == null)// 检查是否存在相同编号图书，存在则不添加
+			if (shelf.add(book)) {
+				differentBookNumber++;
+				flag = true;
+			}
+		return flag;
+	}
+
+	/*
+	 * 通过调用addBook(Book book)实现
+	 */
+	public int addBook(Book[] books) {
+		int successNum = 0;
+		for (Book book : books) {
+			if (addBook(book)) {
+				successNum++;
+			}
+
+		}
+		return successNum;
+	}
+
+	public boolean removeBook(long bookId) {
+		boolean flag = false;
+
+		Book book = getBook(bookId);
+		if (book != null) {
+			shelf.remove(book);
 			flag = true;
 		}
 		return flag;
 	}
 
-	public int addBook(Book[] books) {
-		int successNum = 0;
-		for (Book book : books) {
-			if (shelf.add(book)) {
-				successNum++;
-				differentBookNumber++;
-			}
-		}
-		return successNum;
-	}
-
-	public boolean removeBook(int bookId) {
-		boolean flag = false;
+	private Book getBook(long bookId) {
 
 		Iterator<Book> iterator = shelf.iterator();
-
+		Book book = null;
 		while (iterator.hasNext()) {
 			if (iterator.next().getBookId() == bookId) {
-				iterator.remove();
-				differentBookNumber--;
-				flag = true;
+				book = (Book) iterator;
 				break;
 			}
-			
 		}
-		return flag;
+		return book;
+
 	}
 }
