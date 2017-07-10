3a58948ef51e655985b6e61cf5ce4070a0fee161
logic <923132714@qq.com>
2017-07-10 20:45:21 +0800

    Add getBook ,removeBook and emultfyBook  method by bookName
    增加了通过图书名称获取book对象，修改对象，删除对象。

--- src/com/bean/BookShelf.java
+++ src/com/bean/BookShelf.java
@@ -37,7 +37,9 @@ public class BookShelf {
 		}
 		return successNum;
 	}
-
+	/*
+	 * 通过图书编号获取Book对象
+	 */
 	private Book getBook(long bookId) {
 
 		boolean flag = false;
@@ -56,23 +58,33 @@ public class BookShelf {
 		return null;
 
 	}
+	/*
+	 * 通过图书名称获取Book对象
+	 */
+	private Book getBook(String bookName) {
 
+		boolean flag = false;
+		Iterator<Book> iterator = shelf.iterator();
+		Book book = null;
+		while (iterator.hasNext()) {
+
+			if ((book = iterator.next()).getBookName() == bookName) {
+				flag = true;
+				break;
+			}
+
+		}
+		if (flag)
+			return book;
+		return null;
+
+	}
+	/*
+	 * 获取书架上的book个数
+	 */
 	public int getDifferentBookNumber() {
 		return differentBookNumber;
 	}
-
-	public boolean modifyBook(long bookId, double newBookPrice) {
-		boolean flag = false;
-		Book book = getBook(bookId);
-		if (book != null) {// 验证原书存在不存在
-
-			book.setBookPrice(newBookPrice);
-
-			flag = true;
-		}
-		return flag;
-	}
-
 	public boolean modifyBook(long bookId, double newBookPrice,
 			int newBookAmount) {
 		boolean flag = false;
@@ -85,7 +97,6 @@ public class BookShelf {
 		}
 		return flag;
 	}
-
 	public boolean modifyBook(long bookId, double newBookPrice,
 			int newBookAmount, String newBookBrief) {
 		boolean flag = false;
@@ -98,8 +109,31 @@ public class BookShelf {
 		}
 		return flag;
 	}
+	public boolean modifyBook(String bookName, double newBookPrice,
+			int newBookAmount) {
+		boolean flag = false;
+		Book book = getBook(bookName);
+		if (book != null) {// 验证原书存在不存在
+			book.setBookAmount(newBookAmount);
+			book.setBookPrice(newBookPrice);
 
-	public boolean modifyBook(long bookId, int newBookAmount) {
+			flag = true;
+		}
+		return flag;
+	}
+	public boolean modifyBook(String bookName, double newBookPrice,
+			int newBookAmount, String newBookBrief) {
+		boolean flag = false;
+		Book book = getBook(bookName);
+		if (book != null) {// 验证原书存在不存在
+			book.setBookAmount(newBookAmount);
+			book.setBookPrice(newBookPrice);
+			book.setBookBrief(newBookBrief);
+			flag = true;
+		}
+		return flag;
+	}
+	public boolean modifyBookAmount(long bookId, int newBookAmount) {
 		boolean flag = false;
 		Book book = getBook(bookId);
 		if (book != null) {// 验证原书存在不存在
@@ -109,8 +143,17 @@ public class BookShelf {
 		}
 		return flag;
 	}
+	public boolean modifyBookAmount(String bookName, int newBookAmount) {
+		boolean flag = false;
+		Book book = getBook(bookName);
+		if (book != null) {// 验证原书存在不存在
+			book.setBookAmount(newBookAmount);
 
-	public boolean modifyBook(long bookId, String newBookBrief) {
+			flag = true;
+		}
+		return flag;
+	}
+	public boolean modifyBookBookBrief(long bookId, String newBookBrief) {
 		boolean flag = false;
 		Book book = getBook(bookId);
 		if (book != null) {// 验证原书存在不存在
@@ -120,11 +163,62 @@ public class BookShelf {
 		}
 		return flag;
 	}
+	public boolean modifyBookBookBrief(String bookName, String newBookBrief) {
+		boolean flag = false;
+		Book book = getBook(bookName);
+		if (book != null) {// 验证原书存在不存在
 
+			book.setBookBrief(newBookBrief);
+			flag = true;
+		}
+		return flag;
+	}
+	public boolean modifyBookPrice(long bookId, double newBookPrice) {
+		boolean flag = false;
+		Book book = getBook(bookId);
+		if (book != null) {// 验证原书存在不存在
+
+			book.setBookPrice(newBookPrice);
+
+			flag = true;
+		}
+		return flag;
+	}
+	/*
+	 * 修改book信息
+	 */
+	public boolean modifyBookPrice(String bookName, double newBookPrice) {
+		boolean flag = false;
+		Book book = getBook(bookName);
+		if (book != null) {// 验证原书存在不存在
+
+			book.setBookPrice(newBookPrice);
+
+			flag = true;
+		}
+		return flag;
+	}
+	/*
+	 * 删除shelf中图书编号为bookId的book
+	 */
 	public boolean removeBook(long bookId) {
 		boolean flag = false;
 
 		Book book = getBook(bookId);
+		if (book != null) {
+			shelf.remove(book);
+			flag = true;
+		}
+		return flag;
+	}
+	
+	/*
+	 * 删除shelf中图书名称为bookName的book
+	 */
+	public boolean removeBook(String bookName) {
+		boolean flag = false;
+
+		Book book = getBook(bookName);
 		if (book != null) {
 			shelf.remove(book);
 			flag = true;
