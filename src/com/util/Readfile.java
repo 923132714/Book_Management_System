c395551ce53ce3321fc74050ec09f8f030f65b66
logic <923132714@qq.com>
2017-07-13 11:47:18 +0800

    Add file access
    添加了文件读取

---
+++ src/com/util/Readfile.java
@@ -0,0 +1,59 @@
+package com.util;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.ObjectInputStream;
+import java.util.LinkedList;
+
+public class Readfile<T> {
+	private LinkedList<T> linkedList;
+	
+		
+	public LinkedList<T> readLinkedList(String filePath ) {
+		File file = new File(filePath);
+		if (!file.exists())
+			return null;
+		try {
+			ObjectInputStream in = new ObjectInputStream(new FileInputStream(filePath));
+			 linkedList= (LinkedList<T>)in.readObject();
+			 in.close();
+			} catch (FileNotFoundException e) {
+			e.printStackTrace();
+			} catch (IOException e) {
+			e.printStackTrace();
+			} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+			}
+
+		return linkedList;
+	}
+
+public String  readfile(String filePath ) {
+	File file = new File(filePath);
+	if (!file.exists())
+		return null;
+	StringBuilder stringBuilder =new StringBuilder();
+	String info =null;
+	try {
+		InputStreamReader inputStreamReader = new InputStreamReader(new FileInputStream(filePath));
+		BufferedReader bufferedReader =new BufferedReader(inputStreamReader);
+		while ((info = bufferedReader.readLine()) != null) {
+			stringBuilder.append(info) ;
+			}
+			 info =stringBuilder.toString(); 
+			 bufferedReader.close();
+		} catch (FileNotFoundException e) {
+		e.printStackTrace();
+		} catch (IOException e) {
+		e.printStackTrace();
+		} 
+	return info;
+	}
+}
+	
+
---
+++ src/com/util/WriteFile.java
@@ -0,0 +1,62 @@
+package com.util;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.io.OutputStreamWriter;
+import java.io.UnsupportedEncodingException;
+import java.util.LinkedList;
+
+import javax.xml.ws.handler.MessageContext;
+
+public class WriteFile<T> {
+
+
+	public WriteFile(String filePath, LinkedList<T> linkedList)throws NoSuchFieldException {
+		File file = new File(filePath);
+		try {
+			if (!file.exists())
+				if (!file.createNewFile())
+					throw new NoSuchFieldException();	
+
+			ObjectOutputStream out = new ObjectOutputStream(
+					new FileOutputStream(filePath));
+			out.writeObject(linkedList);
+			out.flush();
+			out.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	public WriteFile(String filePath, String message ,boolean flag)throws NoSuchFieldException {
+
+		File file = new File(filePath);
+		OutputStreamWriter outputStreamWriter;
+		try {
+			if (!file.exists())
+				if (!file.createNewFile())
+					throw new NoSuchFieldException();	
+			outputStreamWriter = new OutputStreamWriter(new FileOutputStream(filePath,flag), "utf-8");
+			BufferedWriter bufferedWriter = new BufferedWriter(outputStreamWriter);
+		if(flag)
+			bufferedWriter.append(message);
+		else
+			bufferedWriter.write(message);
+		bufferedWriter.close();
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+	}
+
+
+}
